c5df344557e01624355619cb240e724d620bed5a
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-08-02 12:42:00 -0600

    docs: Update function definitions and add heroes array

--- 01-bases/src/bases/04-functions.ts
+++ 01-bases/src/bases/04-functions.ts
@@ -1,7 +1,8 @@
+// Definition of a tradicional function
 function greetPerson(name: String) {
   return `Hello, ${name}!`;
 }
-
+// Definition of a funtion using arrow function syntax
 const greetPerson2 = (name: String) => {
   return `Hello, ${name}!`;
 };
@@ -9,7 +10,21 @@ const greetPerson3 = (name: String) => `Hello, ${name}!`;
 
 const returnPersonObject = () => ({ name: "Mexz", age: 30 });
 
+const heroes = [
+  {
+    id: 1,
+    name: "Batman",
+  },
+  {
+    id: 2,
+    name: "Superman",
+    superPower: "Flight",
+  },
+];
+
 console.log(greetPerson("Mexz"));
 console.log(greetPerson2("Mexz"));
 console.log(greetPerson3("Mexz"));
 console.log(returnPersonObject());
+
+console.log(heroes.find((hero) => hero?.id === 1));
