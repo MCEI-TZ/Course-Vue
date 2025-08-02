00945887deb972b496d43011d7e6ffd14b7f1770
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-08-02 16:14:46 -0600

    docs: Add async function to retrieve hero by ID with error handling

--- 01-bases/src/bases/07-imp-exp.ts
+++ 01-bases/src/bases/07-imp-exp.ts
@@ -6,4 +6,8 @@ const getOwnerbyHero = (owner: Owner) => {
   return heroes.filter((heroe) => heroe.owner === owner);
 };
 
+export const getHeroById = (id: number) => {
+  return heroes.find((heroe) => heroe.id === id);
+};
+
 console.log(getOwnerbyHero("DC"));
--- 01-bases/src/bases/08-promises.ts
+++ 01-bases/src/bases/08-promises.ts
@@ -1,3 +1,6 @@
+import type { Hero } from "../data/heroes";
+import { getHeroById } from "./07-imp-exp";
+
 console.log("Start");
 
 new Promise((resolve, reject) => {
@@ -15,3 +18,22 @@ new Promise((resolve, reject) => {
   });
 
 console.log("End");
+// -----------------------------------------------------------------------
+
+const HeroByAsync = (id: number): Promise<Hero> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const hero = getHeroById(id);
+
+      return hero ? resolve(hero) : reject(`Hero with id ${id} not found`);
+    },1000);
+  });
+};
+
+HeroByAsync(5)
+  .then((hero) => {
+    console.log("Hero found:", hero.name);
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
