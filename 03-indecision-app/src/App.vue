506566f6a925ef5ce5e3e8fe35a7522de236bea6
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-08-04 15:31:16 -0600

    feat: Add an example of how to import the logic of a script in our vue file

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript2 from './components/MyCounterScrip2.vue';
 </script>
 
 <template>
   <h1>My fisrt app in Vue</h1>
   <hr />
   <MyCounter :valueProps="1" />
+  <MyCounterScript2 :valueProp="2" />
 </template>
