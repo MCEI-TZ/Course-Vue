5bffed880d71bfb6638509a13fc5f68f303d3dc4
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-08-06 10:55:25 -0600

    feat: Pass messages prop to ChatMessages component for message display

--- 04-ChatBot!Ai/src/view/IndecitionView.vue
+++ 04-ChatBot!Ai/src/view/IndecitionView.vue
@@ -4,12 +4,33 @@
     <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
       <span>Mi esposa</span>
     </div>
-    <ChatMessages />
+    <ChatMessages :messages="messages" />
     <MessageBox />
   </div>
 </template>
 
 <script lang="ts" setup>
-import ChatMessages from '@/components/chat/ChatMessages.vue'
-import MessageBox from '@/components/chat/MessageBox.vue'
+import ChatMessages from '@/components/chat/ChatMessages.vue';
+import MessageBox from '@/components/chat/MessageBox.vue';
+import type { ChatMessage } from '@/interfaces/chatmessages.interfaces';
+import { ref } from 'vue';
+
+const messages = ref<ChatMessage[]>([
+  {
+    id: new Date().getTime(),
+    message: 'Hola mundo',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime(),
+    message: 'Salimos a comer?',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'No',
+    itsMine: false,
+    image: 'https://yesno.wtf/assets/no/3-80a6f5b5d6684674bcfeda34accca4e1.gif',
+  },
+]);
 </script>
